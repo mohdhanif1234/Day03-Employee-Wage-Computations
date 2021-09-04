00edf1134549e6b1700544be32c7d8dbdd038e75
mohdhanif1234 <hanif.topia@gmail.com>
2021-09-04 06:53:39 +0530

    [Hanif]Calculated employee monthly wage till a condition of 100 hours or 20 working days is reached

--- Employee-Wage-Computation/EmployeeWageComputation.cs
+++ Employee-Wage-Computation/EmployeeWageComputation.cs
@@ -13,7 +13,7 @@ namespace Employee_Wage_Computation
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
-
+        public const int MAX_HRS_IN_MONTH = 100;
         static void Main(string[] args)
         {
             
@@ -21,8 +21,11 @@ namespace Employee_Wage_Computation
             int empHrs = 0;
             int empWage = 0;
             int totalEmpWage = 0;
-            for(int day=1; day<=NUM_OF_WORKING_DAYS; day++)
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 //Computation
                 int empCheck = random.Next(0, 3);
@@ -40,9 +43,10 @@ namespace Employee_Wage_Computation
                 }
                 empWage = empHrs * EMP_RATE_PER_HOUR;
                 totalEmpWage += empWage;
-                Console.WriteLine("Employee Wage : Rs " + empWage);
+                Console.WriteLine("Day" + totalWorkingDays + ": " + " Employee Hours : " + empHrs);
                 Console.ReadLine();
             }
+            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Employee Wage : Rs " + totalEmpWage);
             Console.ReadLine();
         }
